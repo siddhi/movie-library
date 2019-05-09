565328c6795dd42d70ae24f41f82245272ff3700
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2019-05-09 23:29:33 +0530

    Made the table fields generic

--- src/components/MovieTable.js
+++ src/components/MovieTable.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function MovieRow({ movie }) {
+function MovieRow({ movie, fields }) {
   return (
     <tr>
-      <td>{movie['Title']}</td><td>{movie['Genre']}</td><td>{movie['Rating']}</td>
+      { fields.map(field => <td>{movie[field]}</td>) }
     </tr>
   );
 }
@@ -23,24 +23,21 @@ function ColumnHeader({ name, sorted, onClick }) {
 }
 
 export default function MovieTable({ movies, sortField, onSort }) {
+  let createColumnHeader = (field) => (
+    <ColumnHeader name={field} sorted={sortField === field} onClick={() => onSort(field)} key={field} />
+  );
+  let fields = ['Title', 'Genre', 'Rating'];
   let sortedMovies = [...movies].sort((a, b) => a[sortField] < b[sortField] ? -1 : 1);
+  
   return (
       <table className="table is-fullwidth">
         <thead>
           <tr>
-            <ColumnHeader name="Title" sorted={sortField === 'Title'} 
-              onClick={() => onSort('Title')}
-            />
-            <ColumnHeader name="Genre" sorted={sortField === 'Genre'} 
-              onClick={() => onSort('Genre')}
-            />
-            <ColumnHeader name="Rating" sorted={sortField === 'Rating'} 
-              onClick={() => onSort('Rating')}
-            />
+            { fields.map((fieldName) => createColumnHeader(fieldName)) }
           </tr>
         </thead>
         <tbody>
-          { sortedMovies.map((movie) => <MovieRow movie={movie} key={movie['Title']}/>) }
+          { sortedMovies.map((movie) => <MovieRow movie={movie} key={movie['Title']} fields={fields}/>) }
         </tbody>
       </table>
   );
