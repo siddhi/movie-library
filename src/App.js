b442355b88a63eaa1a325a0551f2fe3cf92540b9
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2019-05-09 23:06:50 +0530

    Added sorting functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import 'bulma/css/bulma.css';
 import MovieTable from './components/MovieTable';
 import movieList from './movies.json';
 
+let dedupedMovieList = [];
+movieList.forEach((movie) => {
+  if (dedupedMovieList.findIndex((m) => m['Title'] === movie['Title']) === -1) {
+    dedupedMovieList.push(movie);
+  }
+});
+let cleanedMovieList = dedupedMovieList
+  .map((movie) => ({ 
+    'Title': movie['Title'], 
+    'Genre': movie['Major_Genre'] || 'Unknown', 
+    'Rating': movie['IMDB_Rating'] || 0 
+  }));
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: movieList
+      movies: cleanedMovieList,
+      sortField: "Title"
     }
   }
 
@@ -16,7 +30,11 @@ class App extends React.Component {
       <div className="container">
         <div className="columns">
           <div className="column">
-            <MovieTable movies={this.state.movies} />
+            <MovieTable
+              movies={this.state.movies}
+              sortField={this.state.sortField}
+              onSort={(sortField) => this.setState({sortField})}
+            />
           </div>
           <div className="column">
           </div>
--- src/components/MovieTable.js
+++ src/components/MovieTable.js
@@ -3,25 +3,46 @@ import React from 'react'
 function MovieRow({ movie }) {
   return (
     <tr>
-      <td>{movie['Title']}</td><td>{movie['Major_Genre']}</td><td>{movie['IMDB_Rating']}</td>
+      <td>{movie['Title']}</td><td>{movie['Genre']}</td><td>{movie['Rating']}</td>
     </tr>
   );
 }
 
-export default function MovieTable({ movies, sortField }) {
+function SortIcon() {
+  return (
+    <span className="icon">
+      <i className="fas fa-sort-down"></i>
+    </span>
+  )
+}
+
+function ColumnHeader({ name, sorted, onClick }) {
+  return (
+    <td onClick={onClick}>{ name } { sorted ? <SortIcon /> : null }</td>
+  );  
+}
+
+export default function MovieTable({ movies, sortField, onSort }) {
   let sortedMovies = [...movies].sort((a, b) => a[sortField] < b[sortField] ? -1 : 1);
   return (
       <table className="table is-fullwidth">
         <thead>
-          <tr><td>Title</td><td>Genre</td><td>Rating</td></tr>
+          <tr>
+            <ColumnHeader name="Title" sorted={sortField === 'Title'} 
+              onClick={() => onSort('Title')}
+            />
+            <ColumnHeader name="Genre" sorted={sortField === 'Genre'} 
+              onClick={() => onSort('Genre')}
+            />
+            <ColumnHeader name="Rating" sorted={sortField === 'Rating'} 
+              onClick={() => onSort('Rating')}
+            />
+          </tr>
         </thead>
         <tbody>
-          { sortedMovies.map((movie) => <MovieRow movie={movie} />) }
+          { sortedMovies.map((movie) => <MovieRow movie={movie} key={movie['Title']}/>) }
         </tbody>
       </table>
   );
 }
 
-MovieTable.defaultProps = {
-  sortField: 'Title'
-}
